12daca18f9fc0b8cb558112d148259de49470cdc
moura-dev <junior.moura19@hotmail.com>
2022-04-09 17:02:26 -0300

    Creating CompanySerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,15 @@
-from core.models import Seller
 from rest_framework import serializers
+
+from core.models import Seller, Company
 
 
 class SellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seller
+        fields = "__all__"
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
         fields = "__all__"
