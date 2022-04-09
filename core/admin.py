bbb66d37a89fbc8caf16fe4d7988ae60f0dd01c2
moura-dev <junior.moura19@hotmail.com>
2022-04-09 17:01:35 -0300

    creating and register models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Company, Seller, Portage, Customer, Factory, Employer, Product, Factory_Product, Order, OrderItems
+
 # Register your models here.
+
+
+admin.site.register(Company)
+admin.site.register(Seller)
+admin.site.register(Portage)
+admin.site.register(Customer)
+admin.site.register(Factory)
+admin.site.register(Employer)
+admin.site.register(Product)
+admin.site.register(Factory_Product)
+admin.site.register(Order)
+admin.site.register(OrderItems)
