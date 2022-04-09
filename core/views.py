ee3dab10a4120e6d751b71547a33eaa493ab0f07
moura-dev <junior.moura19@hotmail.com>
2022-04-09 17:03:13 -0300

    Creating CompanyViewSet

--- core/views.py
+++ core/views.py
@@ -1,21 +1,8 @@
-from operator import le
-from urllib import request
-from django.http import JsonResponse
-from django.shortcuts import render
-
 # Create your views here.
-from django.views import View
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, generics, status, filters
-from rest_framework.renderers import JSONRenderer
-from rest_framework.views import APIView
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import GenericAPIView, get_object_or_404
 
-from core.models import Seller
-from core.serializers import SellerSerializer
+from core.models import Seller, Company
+from core.serializers import SellerSerializer, CompanySerializer
 
 
 class SellerViewSet(ModelViewSet):
@@ -23,3 +10,6 @@ class SellerViewSet(ModelViewSet):
     queryset = Seller.objects.all()
 
 
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
