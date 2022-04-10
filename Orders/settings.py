30ed256437f4017342bd8040f467bc83e6ec6134
Ivan Noleto <ivannoleto@hotmail.com>
2022-04-09 21:03:59 -0300

    Add Mysql Fixes #2

--- Orders/settings.py
+++ Orders/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Orders.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_orders',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
